c7c768720a92d7ebf7857178c8cf2f0f6aed040e
IKobozev <IKobozev@synergy.ru>
2026-06-28 18:52:17 +0300

    iter1 разработан сервер и 2 хендлера

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/KiberIGOR/tinyurl/internal/handler"
+)
+func main() {
+	mux:=http.NewServeMux()
+	mux.HandleFunc("/", handler.PostUrlHandler)
+	mux.HandleFunc("/{id}", handler.GetUrlHandler)
+	err := http.ListenAndServe(":8080", mux)
+	if err!= nil {
+		panic(err)
+	}
+}
---
+++ internal/handler/handlers.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func GetUrlHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(res, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	id := req.PathValue("id")
+	url:="https://practicum.yandex.ru/"
+	res.Header().Set("location",url)
+	res.WriteHeader(http.StatusTemporaryRedirect)
+	res.Write([]byte(id))
+}
+
+func PostUrlHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res,"Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	content := req.Header.Get("content-type")
+	if (content != "text/plain")&&(content != "text/plain;charset=UTF-8") {
+		http.Error(res, "Only content-type: text/plain are allowed!", http.StatusBadRequest)
+		return
+	}
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+        http.Error(res, err.Error(), http.StatusBadRequest)
+        return
+  }
+	if len(body)==0 {
+		http.Error(res,"URL is required",http.StatusBadRequest)
+		return
+	}
+	url:="http://localhost:8080/EwHXdJfB"
+	res.Header().Set("content-type", "text/plain")
+	res.Header().Set("content-length", fmt.Sprintf("%d",len(url)))
+	res.WriteHeader(http.StatusCreated)
+	res.Write([]byte(url))
+}
