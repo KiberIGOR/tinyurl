c6130b1b4591b1bdc3261285efbf015f1b398f65
IKobozev <IKobozev@synergy.ru>
2026-06-28 19:00:26 +0300

    iter1 fix теста

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -12,7 +12,7 @@ func GetUrlHandler(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	id := req.PathValue("id")
-	url:="https://practicum.yandex.ru/"
+	url:="http://q4f60inud.net"
 	res.Header().Set("location",url)
 	res.WriteHeader(http.StatusTemporaryRedirect)
 	res.Write([]byte(id))
