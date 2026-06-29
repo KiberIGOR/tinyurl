7b1fa8dd82cfe2517308a98b28f86183d904e5af
IKobozev <IKobozev@synergy.ru>
2026-06-29 17:50:53 +0300

    iter1 добавлено сохранение url в общий map[string]string

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/KiberIGOR/tinyurl/internal/handler"
 )
+
 func main() {
-	mux:=http.NewServeMux()
-	mux.HandleFunc("/", handler.PostUrlHandler)
-	mux.HandleFunc("/{id}", handler.GetUrlHandler)
+	urls := make(map[string]string)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler.PostUrlHandler(&urls))
+	mux.HandleFunc("/{id}", handler.GetUrlHandler(&urls))
 	err := http.ListenAndServe(":8080", mux)
-	if err!= nil {
+	if err != nil {
 		panic(err)
 	}
 }
