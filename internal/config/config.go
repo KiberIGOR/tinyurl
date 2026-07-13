2090a4875b8aed3be229410f16279a18fc01f6f8
IKobozev <IKobozev@synergy.ru>
2026-07-13 15:33:37 +0300

    iter5 добалевны переменные окружения

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 type Config struct {
 	Address string
@@ -12,5 +15,14 @@ func Parse() *Config {
 	flag.StringVar(&cfg.Address, "a", ":8080", "address and port to run server")
 	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "address and port for redirect url")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+        cfg.Address = envRunAddr
+  }
+	
+	if envRunAddr := os.Getenv("BASE_URL"); envRunAddr != "" {
+        cfg.BaseURL = envRunAddr
+  }
+
 	return cfg
 }
