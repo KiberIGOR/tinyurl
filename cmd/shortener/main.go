1ef9daeef63991f874613998424addd0fc5c6a70
IKobozev <IKobozev@synergy.ru>
2026-07-17 17:41:53 +0300

    iter6 добавлено логирование http методов через middleware

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"net/http"
 
+	"log"
+
 	"github.com/KiberIGOR/tinyurl/internal/config"
 	"github.com/KiberIGOR/tinyurl/internal/handler"
+	"github.com/KiberIGOR/tinyurl/internal/logger"
 	"github.com/KiberIGOR/tinyurl/internal/repository"
 	"github.com/KiberIGOR/tinyurl/internal/service"
 	"github.com/go-chi/chi/v5"
-	"log"
 )
 
 func main() {
 	cfg := config.Parse()
+
+	if err:=logger.Initialize("info");err!=nil {
+		log.Fatal(err)
+	}
 
 	store := repository.NewMemory()
 	svc := service.NewShortener(store, cfg.BaseURL)
 	h := handler.New(svc)
 
 	r := chi.NewRouter()
-	r.Post("/", h.PostUrlHandler)
-	r.Get("/{id}", h.GetURL)
+	r.Post("/", logger.RequestLogger(h.PostUrlHandler))
+	r.Get("/{id}",logger.RequestLogger(h.GetURL))
 	err := http.ListenAndServe(cfg.Address, r)
 	if err != nil {
 		log.Fatal(err)
