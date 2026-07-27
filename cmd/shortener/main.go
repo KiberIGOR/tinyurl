e988ebe0c5883a429401efacae43a65e0d2ef179
IKobozev <IKobozev@synergy.ru>
2026-07-27 04:11:33 +0300

    iter8 добалена middleware компрессия

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/KiberIGOR/tinyurl/internal/logger"
 	"github.com/KiberIGOR/tinyurl/internal/repository"
 	"github.com/KiberIGOR/tinyurl/internal/service"
+	"github.com/KiberIGOR/tinyurl/internal/compress"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -25,9 +26,9 @@ func main() {
 	h := handler.New(svc)
 
 	r := chi.NewRouter()
-	r.Post("/", logger.RequestLogger(h.PostUrlHandler))
-	r.Post("/api/shorten", logger.RequestLogger(h.PostJsonUrlHandler))
-	r.Get("/{id}",logger.RequestLogger(h.GetURL))
+	r.Post("/", logger.RequestLogger(compress.GzipMiddleware(h.PostUrlHandler)))
+	r.Post("/api/shorten", logger.RequestLogger(compress.GzipMiddleware(h.PostJsonUrlHandler)))
+	r.Get("/{id}",logger.RequestLogger(compress.GzipMiddleware(h.GetURL)))
 	err := http.ListenAndServe(cfg.Address, r)
 	if err != nil {
 		log.Fatal(err)
